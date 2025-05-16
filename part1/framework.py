08def63ea99fd2593c60ae25d1b0699383e41cbd
aroy23 <arnavroy@ucla.edu>
2025-05-16 13:07:41 -0700

    renamed vars

--- part1/framework.py
+++ part1/framework.py
@@ -20,18 +20,18 @@ submit = st.button("Submit", disabled=(not user_prompt))
 
 # create sidebar for API keys
 st.sidebar.header("API Keys")
-openai_api_key = st.sidebar.text_input("Google API Key", type="password")
+google_api_key = st.sidebar.text_input("Google API Key", type="password")
 tavily_api_key = st.sidebar.text_input("Tavily API Key", type="password")
 
 # takes the keys the user typed in and sets them as environment variables
 # necessary when using langchain and tavily because they read keys from the environment variables
-if openai_api_key:
-    os.environ["GOOGLE_API_KEY"] = openai_api_key
+if google_api_key:
+    os.environ["GOOGLE_API_KEY"] = google_api_key
 if tavily_api_key:
     os.environ["TAVILY_API_KEY"] = tavily_api_key
 
 # check if API keys are set, if not, show warning message and stop execution
-if not openai_api_key or not tavily_api_key:
+if not google_api_key or not tavily_api_key:
     st.warning("Please enter your API keys in the sidebar to continue.")
     st.stop()
 
